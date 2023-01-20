468a557b2b3019ee575ed4738e7ee58ac7d49db1
Dolfost <rehanvladyslav@gmail.com>
2023-01-20 14:18:40 +0200

    add -s key

--- main.cpp
+++ main.cpp
@@ -9,7 +9,14 @@ int main(int argc, char** argv) {
   
   signed char* option = (signed char*)malloc(sizeof(signed char));
 
-  while ((*option = getopt(argc, argv, ":?hvi")) != -1) {
+  bool sizeopt = false;
+
+  char* filepath = NULL;
+
+  long double filesize = 0;
+  char* filesizeunits = (char*)malloc(3*sizeof(char));
+
+  while ((*option = getopt(argc, argv, ":?sf:hv")) != -1) {
     switch (*option) {
       case 'h': {
                   printf("This is a help.\n");
@@ -27,8 +34,38 @@ int main(int argc, char** argv) {
                   fprintf(stderr, "Unknown option %s found.\n", argv[optind-1]);
                   exit(EXIT_FAILURE);
                 }
+      case 'f': {
+                  if (filepath != NULL) {
+                    free(filepath);
+                    filepath = NULL;
+                  }
+                  filepath = (char*)malloc((strlen(optarg)+1)*sizeof(char*));
+                  if (filepath == NULL) {
+                    fprintf(stderr, "Could not allocate memory.\n");
+                  }
+                  memcpy(filepath, optarg, strlen(optarg));
+                  filepath[strlen(optarg)] = '\0';
+                  break;
+                } 
+      case 's': {
+                  sizeopt = true;
+                }
     }
+  }
+  
+  free(option);
 
+  if (sizeopt == true) {
+    filesize = getfilesize(filepath, filesizeunits);
+    if (filesize == -1) {
+      fprintf(stderr, "Could not open %s.\n", filepath);
+      exit(EXIT_FAILURE);
+    }
+    if (filesize == -2 || filesize == -3) {
+      fprintf(stderr, "Could not work with file %s.\n", filepath);
+      exit(EXIT_FAILURE);
+    }
+    printf("Size of %s is %Lf%s\n", filepath, filesize, filesizeunits);
   }
 
   return EXIT_SUCCESS;
